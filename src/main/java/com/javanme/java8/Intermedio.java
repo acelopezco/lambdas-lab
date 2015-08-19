a5f3d41c8565adcd6cb2f33c0ae3943ea3704ece
Alexis Lopez <aalopez@gmail.com>
2015-08-18 22:59:39 -0500

    Adiciona ejercicio 1 de nivel intermedio

--- src/main/java/com/javanme/java8/Intermedio.java
+++ src/main/java/com/javanme/java8/Intermedio.java
@@ -1,8 +1,24 @@
 package com.javanme.java8;
 
+import java.nio.file.Path;
+
 /**
  * Clase con ejercicios nivel intermedio
  * Created by aalopez on 18/08/15.
  */
 public class Intermedio {
+
+    /**
+     * Contar el número de líneas no vacías que tiene el archivo pasado por parámetro
+     * Usar nuevos métodos encontrados en la clase BufferedReader en Java 8
+     *
+     * @param archivo Ruta al archivo que se desea evaluar
+     * @return Cantidad de líneas en el archivo
+     * @see java.io.BufferedReader
+     * @see java.util.stream.Stream
+     */
+    public long ejercicio1(Path archivo) {
+        throw new UnsupportedOperationException();
+    }
 }
+
--- src/test/java/com/javanme/java8/IntermedioTest.java
+++ src/test/java/com/javanme/java8/IntermedioTest.java
@@ -1,9 +1,26 @@
 package com.javanme.java8;
 
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Pruebas para los ejercicios de nivel intermedio
  * Created by aalopez on 18/08/15.
  */
 public class IntermedioTest {
     private Intermedio ejercicios = new Intermedio();
+
+    @Test
+    public void ejercicio1() {
+        Path archivo = Paths.get("CaliPachanguero.txt");
+        if (Files.exists(archivo)) {
+            long resultado = ejercicios.ejercicio1(archivo);
+            assertEquals(40, resultado);
+        }
+    }
 }
