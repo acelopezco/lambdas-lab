8ce507b53d68f5554c6c1108062daf9689da0e63
Alexis Lopez <aalopez@gmail.com>
2015-08-18 21:11:02 -0500

    Adiciona ejercicio 1 a los ejercicios básicos

--- src/main/java/com/javanme/java8/Basico.java
+++ src/main/java/com/javanme/java8/Basico.java
@@ -1,8 +1,20 @@
 package com.javanme.java8;
 
+import java.util.List;
+
 /**
  * Clase con ejercicios nivel básico
  * Created by aalopez on 18/08/15.
  */
 public class Basico {
+
+    /**
+     * Convertir a mayúsculas cada una de las palabras recibidas como parámetro
+     *
+     * @param palabras Listado de palabras a convertir
+     * @return Lista que contiene las palabras en mayúsculas
+     */
+    public List<String> ejercicio1(List<String> palabras) {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/test/java/com/javanme/java8/BasicoTest.java
+++ src/test/java/com/javanme/java8/BasicoTest.java
@@ -1,9 +1,25 @@
 package com.javanme.java8;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Pruebas para los ejercicios de nivel básico
  * Created by aalopez on 18/08/15.
  */
 public class BasicoTest {
     private Basico ejercicios = new Basico();
+
+    @Test
+    private void ejercicio1() {
+        List<String> listado = Arrays.asList(
+                "Cali", "es", "cali", "lo", "demás", "es", "loma");
+        List<String> resultado = ejercicios.ejercicio1(listado);
+        assertEquals(Arrays.asList(
+                "CALI", "ES", "CALI", "LO", "DEMÁS", "ES", "LOMA"), resultado);
+    }
 }
