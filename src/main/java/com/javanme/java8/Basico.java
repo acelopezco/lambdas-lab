7e041e43a7108c827a101ced5c2b99b6bb410cc6
Alexis Lopez <aalopez@gmail.com>
2015-08-18 21:29:13 -0500

    Adiciona ejercicio 2 a los ejercicios básicos

--- src/main/java/com/javanme/java8/Basico.java
+++ src/main/java/com/javanme/java8/Basico.java
@@ -10,11 +10,23 @@ public class Basico {
 
     /**
      * Convertir a mayúsculas cada una de las palabras recibidas como parámetro
+     * Trata de usar uno de los nuevos métodos adicionados a las listas en Java 8
      *
      * @param palabras Listado de palabras a convertir
      * @return Lista que contiene las palabras en mayúsculas
      */
     public List<String> ejercicio1(List<String> palabras) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Del listado de cadenas de texto eliminar las cadenas de ese listado cuyo tamaño sea inferior a 10 carácteres
+     * Trata de usar uno de los nuevos métodos adicionados a las listas en Java 8
+     *
+     * @param listado cadenas de texto
+     * @return lista que contiene cadenas de texto cuyo tamaño de carácteres es superior a 10
+     */
+    public List<String> ejercicio2(List<String> listado) {
         throw new UnsupportedOperationException();
     }
 }
--- src/test/java/com/javanme/java8/BasicoTest.java
+++ src/test/java/com/javanme/java8/BasicoTest.java
@@ -15,11 +15,20 @@ public class BasicoTest {
     private Basico ejercicios = new Basico();
 
     @Test
-    private void ejercicio1() {
+    public void ejercicio1() {
         List<String> listado = Arrays.asList(
                 "Cali", "es", "cali", "lo", "demás", "es", "loma");
         List<String> resultado = ejercicios.ejercicio1(listado);
         assertEquals(Arrays.asList(
                 "CALI", "ES", "CALI", "LO", "DEMÁS", "ES", "LOMA"), resultado);
+    }
+
+    @Test
+    public void ejercicio2() {
+        List<String> listado = Arrays.asList(
+                "A la una", "De eso tan bueno no dan tanto", "Hoy no fío mañana sí", "A las dos", "Más vale pájaro en mano que cien volando", "Entre menos sepa mejor vivo", "La vejez al que no lo mata lo desfigura", "y", "A las tres", "La plata se va y el burro queda");
+        List<String> resultado = ejercicios.ejercicio2(listado);
+        assertEquals(Arrays.asList(
+                "De eso tan bueno no dan tanto", "Hoy no fío mañana sí", "Más vale pájaro en mano que cien volando", "Entre menos sepa mejor vivo", "La vejez al que no lo mata lo desfigura", "La plata se va y el burro queda"), resultado);
     }
 }
