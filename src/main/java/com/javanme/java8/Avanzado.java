b0ab4be2130a511ae2fe7e17a133af09dcf21287
Alexis Lopez <aalopez@gmail.com>
2017-09-13 16:13:52 -0500

    Adiciona ejercicios 1 y 2 a la lista de ejercicios avanzados.
    Se adicionan dos nuevos ejercicios avanzados que buscan que el desarrollador haga uso del método reduce de la API Stream.

--- src/main/java/com/javanme/java8/Avanzado.java
+++ src/main/java/com/javanme/java8/Avanzado.java
@@ -1,8 +1,35 @@
 package com.javanme.java8;
 
+import java.util.List;
+
+
 /**
  * Clase con ejercicios nivel avanzado
  * Created by aalopez on 18/08/15.
  */
 public class Avanzado {
+
+    /**
+     * Realice la suma de los números pasados por param.
+     * Use la API Stream y el método {@code +reduce(T, BinaryOperator<T>):T} para definir la forma en la cual se va
+     * a reducir el stream a un solo valor (la suma de todos sus números).
+     *
+     * @param numeros Listado de números a sumar.
+     * @return Suma de los números pasados por param.
+     */
+    public int ejercicio1(List<Integer> numeros) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Obtenga el máximo de los números pasados por param
+     * Use la API Stream y el método {@code +reduce(BinaryOperator<T>):Optional<T>} para definir la forma en la cual se va
+     * a reducir el stream a un solo valor (el máximo de los números).
+     *
+     * @param numeros Listado de números.
+     * @return El máximo de los números pasados por param.
+     */
+    public long ejercicio2(List<Long> numeros) {
+        throw new UnsupportedOperationException();
+    }
 }
--- src/test/java/com/javanme/java8/AvanzadoTest.java
+++ src/test/java/com/javanme/java8/AvanzadoTest.java
@@ -1,9 +1,34 @@
 package com.javanme.java8;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Pruebas para los ejercicios de nivel avanzado
  * Created by aalopez on 18/08/15.
  */
 public class AvanzadoTest {
     private Avanzado ejercicios = new Avanzado();
+
+    @Test
+    public void ejercicio1() {
+
+        List<Integer> numeros = Arrays.asList(1, 1, 2, 3, 5, 8, 13, 21);
+        int suma = ejercicios.ejercicio1(numeros);
+
+        assertTrue(54 == suma);
+    }
+
+    @Test
+    public void ejercicio2() {
+
+        List<Long> numeros = Arrays.asList(1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L);
+        long max = ejercicios.ejercicio2(numeros);
+
+        assertTrue(21L == max);
+    }
 }
